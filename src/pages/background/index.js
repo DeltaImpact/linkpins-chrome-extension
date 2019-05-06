3c3eebaa78f1676c61579050a79db814f1289f33
DeltaImpact <p.grachev.vik@gmail.com>
2019-05-06 18:19:26 +0300

    commented background

--- src/pages/background/index.js
+++ src/pages/background/index.js
@@ -1,10 +1,18 @@
-import store from './store';
-import {incrementBackgroundCounter, decrementBackgroundCounter} from '../../store/actions/background/actions';
+// import store from './store';
+// import {incrementBackgroundCounter, decrementBackgroundCounter} from '../../store/actions/background/actions';
+// import {incrementBackgroundCounter, decrementBackgroundCounter} from '../../store/actions/popup/actions';
 
-// increment or decrement background counter every second
-setInterval(() => {
-    store.dispatch(Math.random() >= 0.5 ?
-        incrementBackgroundCounter() :
-        decrementBackgroundCounter()
-    );
-}, 1000);
+// import {Store, applyMiddleware} from 'webext-redux';
+// import logger from "redux-logger";
+
+// const store = new Store();
+// const middleware = [logger];
+// const storeWithMiddleware = applyMiddleware(store, ...middleware);
+
+// // increment or decrement background counter every second
+// setInterval(() => {
+//     storeWithMiddleware.dispatch(Math.random() >= 0.5 ?
+//         incrementBackgroundCounter() :
+//         decrementBackgroundCounter()
+//     );
+// }, 1000);
